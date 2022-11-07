f7c42512cd8e686b3019a25aa6f165d7ee84f676
BeSmile <l735465519@gmail.com>
2022-11-07 16:19:55 +0800

    ci: 增加stylelint校验以及调整lint-staged

---
+++ lint-staged.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    '*.{js,jsx,less,md,json}': ['prettier --write'],
+    '*.ts?(x)': ['prettier --parser=typescript --write', 'eslint --quiet'],
+    '*.less': ['stylelint']
+};
