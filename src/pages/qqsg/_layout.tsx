850ee514d9a1c82e82da04fd1057e85a4efb43b0
BeSmile <l735465519@gmail.com>
2023-01-17 16:12:40 +0800

    style: 调整布局

--- src/pages/qqsg/_layout.tsx
+++ src/pages/qqsg/_layout.tsx
@@ -2,10 +2,5 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 export default function _layout() {
-  return (
-    <div>
-      <div>1234</div>
-      <Outlet />
-    </div>
-  );
+  return <Outlet />;
 }
