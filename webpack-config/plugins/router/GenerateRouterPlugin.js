e5b756a50b0fcb686aeaf08e63e1256dc12a9a09
BeSmile <l735465519@gmail.com>
2023-02-21 17:26:05 +0800

    feat: 增加文件变动的监听

--- webpack-config/plugins/router/GenerateRouterPlugin.js
+++ webpack-config/plugins/router/GenerateRouterPlugin.js
@@ -1,9 +1,36 @@
 const generateRouter = require('./generateRouter');
+const gulp = require('gulp');
+const path = require('path');
+const fancyLog = require('fancy-log');
+
+const watchDir = path.join(__dirname, '../../../src/pages');
+
+const watchRouterNamesTask = function() {
+  const watcher = gulp.watch([`${watchDir}/**/*.tsx`]);
+  fancyLog.info(`start watch ${watchDir}`);
+  
+  watcher.on('ready', function () {
+    ['add', 'unlink', 'addDir', 'unlinkDir'].forEach(function(type) {
+      watcher.on(type, function(filePath, stats) {
+        fancyLog.info(`File ${filePath} was ${type}`, stats);
+        generateRouter();
+      });
+    });
+  });
+  
+  return watcher;
+};
 
 class GenerateRouterPlugin {
   apply(compiler) {
-    compiler.hooks.afterEnvironment.tap('GenerateRouterPlugin', function () {
+    compiler.hooks.afterEnvironment.tap('GenerateRouterPlugin', ()=>{
       generateRouter();
+      this.watcher = watchRouterNamesTask();
+    });
+    compiler.hooks.watchClose.tap('GenerateRouterPlugin', () =>  {
+      fancyLog.info('webpack closed');
+      this.watcher.unwatch();
+      this.watcher.close();
     });
   }
 }
